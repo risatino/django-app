3941da48984b7defc9b854e4c4edd9f2df8116d9
Risa Prezzano <charissatolentino@gmail.com>
2018-08-23 22:33:48 +0000

    starter basic django app set-up

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    url(r'^index/$', views.UserListView.as_view(), name='index'),
+
+    url(r'^detail/(?P<pk>[0-9]+)/$', 
+        views.UserDetailView.as_view(), name='detail'),
+
+    url(r'^add/$', views.add, name='add'),
+
+    url(r'^test/$', views.test, name='test'),
+
+    url(r'^testTemplate/$', views.testTemplate, name='testTemplate'),
+]
